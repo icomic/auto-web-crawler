096d8b5259c931d058f2860f1fb957b17864f9cc
icomic <ignmickus@gmail.com>
2018-07-31 13:56:01 +0300

    1. Cleaned up browser init; 2. Added new folder 'Pages'; 3. Added Quick search page;

--- src/browserDriver.js
+++ src/browserDriver.js
@@ -8,10 +8,9 @@ const puppeteer = require('puppeteer');
 
 
 (async () => {
-	const browser = await puppeteer.launch({headless: false});
+	const browser = await puppeteer.launch({headless: false, executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'});
 	const page = await browser.newPage();
 	await page.goto('https://www.autoscout24.com')
-	await page.screenshot({path: 'example.png'});
-
+	
 	await browser.close();
 })();
