d5b529211c1a0765387270ca785bdc51d36bb445
icomic <ignmickus@gmail.com>
2018-07-31 13:42:11 +0300

    "Headless" = false

--- src/main.js
+++ src/main.js
@@ -8,9 +8,9 @@ const puppeteer = require('puppeteer');
 
 
 (async () => {
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({headless: false});
 	const page = await browser.newPage();
-	await page.goto('https://autoplius.lt')
+	await page.goto('https://www.autoscout24.com')
 	await page.screenshot({path: 'example.png'});
 
 	await browser.close();
